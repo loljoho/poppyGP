d4c586c34cf1b51a679dbc65bcb094aa76c25c69
loljoho <jho1290@smail.raritanval.edu>
2015-09-11 09:32:30 -0400

    Redesigned gp-video Directive

--- src/app/components/video/video.directive.js
+++ src/app/components/video/video.directive.js
@@ -12,23 +12,22 @@
       replace: false,
       transclude: false,
       templateUrl: 'app/components/video/video.html',
-      controller: VideoController,
-      controllerAs: 'vm'
-    };
-
-    /** @ngInject */
-    function VideoController() {
-      var vm = this;
-      vm.joho = '';
-
-      activate();
-
-      function activate() {
-        vm.joho = 'Joho';
+      scope: {
+        channelId : '=*?channelId',
+        version   : '=*?playerVersion'
+      },
+      link: function(scope) {
+        scope.videoSrc = 'http://www.ustream.tv/embed/' + scope.channelId + '?';
+        // Use Old Player
+        if(scope.version === 'old') {
+          scope.videoSrc += '?v=3&amp;wmode=direct&showtitle=false';
+        }
+        // New HTML5 Player
+        else {
+          scope.videoSrc += 'html5ui=1&controls=false&showtitle=false';
+        }
       }
-
-    }
-
+    };
   }
 
 })();
